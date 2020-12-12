e497a311715e9ccad02715c9eedc0a44bc9208c0
Clóvis Neto <cloves13@hotmail.com>
2020-12-12 17:19:02 +0100

    Add SWAGGER API

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,22 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const options = new DocumentBuilder()
+    .setTitle('Reddit Newsletter')
+    .setDescription('The reddit newsletter api')
+    .setVersion('1.0')
+    .addTag('reddit-newsletter')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, options);
+
+  SwaggerModule.setup('api', app, document);
+
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(3000);
 }
